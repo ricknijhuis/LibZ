1525a574d6db8acc56ee9fb1ecbb15cdbc586987
rick <40138649+ricknijhuis@users.noreply.github.com>
2025-02-12 17:37:10 +0100

    Started work on list container

--- src/containers/list.zig
+++ src/containers/list.zig
@@ -1,3 +1,13 @@
 const core = @import("core");
+const grow_factors = core.grow_factors;
+const GrowFactor = grow_factors.GrowFactor;
 
-pub const List = struct {};
+pub fn List(T: type, grow_factor: GrowFactor) type {
+    return ListAligned(T, grow_factor, @alignOf(T));
+}
+
+pub fn ListAligned(T: type, grow_factor: GrowFactor, alignment: u29) type {
+    return struct {
+        const Self = @This();
+    };
+}
